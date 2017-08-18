d9ea36611527cc8c2022f59243e17f6165538eb8
rodrigotimm <31116951+rodrigotimm@users.noreply.github.com>
2017-08-17 22:24:18 -0300

    add sysout

--- Academico/src/Pincipal.java
+++ Academico/src/Pincipal.java
@@ -3,15 +3,17 @@ public class Pincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Aluno a1 = new Aluno ("Rodrigo Timm", "21709007 ", 22 , " Computação", "noturno", "Masculino"  );		
+		Aluno a1 = new Aluno ("Rodrigo Timm", "21709007 ", 22 , " ComputaÃ§Ã£o", "noturno", "Masculino"  );		
 		
 	
-	        Disciplina d1 = new Disciplina ("TGA", " Computação", 75 );
+	        Disciplina d1 = new Disciplina ("TGA", " ComputaÃ§Ã£o", 75 );
 	        
 	          System.out.println(a1.getNome());
 	          System.out.println(d1.getNome());
 	          
 	          System.out.println(a1.getCurso());
+		
+		   System.out.println(a1.getIdade());
 	          
 	          System.out.println(a1.imprimirAluno());
 	          
